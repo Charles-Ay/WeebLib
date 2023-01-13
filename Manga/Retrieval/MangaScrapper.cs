c2b63f924657856605562673dead32140077df40
Charles Ayeni <charles.work20@gmail.com>
2023-01-13 17:06:32 -0500

    Moved download of image to method

--- Manga/Retrieval/MangaScrapper.cs
+++ Manga/Retrieval/MangaScrapper.cs
@@ -8,12 +8,36 @@ using WeebLib.Manga.Parser;
 using WeebLib.Novel;
 using WeebLib.Manga.MangaExceptions;
 using System.Net;
+using System.ComponentModel;
+using System.Threading;
 
 namespace WeebLib.Manga.Retrieval
 {
     internal class MangaScrapper : IWebScrapper<MangaData>
     {
         protected string? mangaText;
+
+        private void DownloadImage(string image, string dir, MangaData manga, int imgNumber)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(image);
+            using (HttpWebResponse response = (HttpWebResponse) request.GetResponse())
+            {
+                using (Stream stream = response.GetResponseStream())
+                {
+                    using (FileStream fileStream = new FileStream(Path.Combine(dir, $"{manga.name} - Chapter {manga.latestChapter}_{imgNumber}.jpg"), FileMode.Create, FileAccess.Write))
+                    {
+                        byte[] buffer = new byte[4096];
+                        int bytesRead;
+                        do
+                        {
+                            bytesRead =  stream.Read(buffer, 0, buffer.Length);
+                             fileStream.Write(buffer, 0, bytesRead);
+                        } while (bytesRead != 0);
+                    }
+                }
+            }
+        }
+
         public override string Scrape(List<MangaData> data, string dir, bool outputToFile = true)
         {
             List<string> images = new List<string>();
@@ -21,9 +45,19 @@ namespace WeebLib.Manga.Retrieval
             //Check if the "Manga" Dir exist. If not make a new one
             if (!Directory.Exists(Path.Combine(dir, "Manga"))) Directory.CreateDirectory(Path.Combine(dir, "Manga"));
             var mangaDir = Path.Combine(dir, "Manga");
+            
+            //BackgroundWorker worker = new BackgroundWorker();
+            //worker.DoWork += (sender, e) =>
+            //{
+            //    int imgNumber = 1;
+            //    foreach (string image in images)
+            //    {
+            //        DownloadImage(image, dir, manga, imgNumber);
+            //        ++imgNumber;
+            //    }
+            //};
 
-
-
+            //worker.RunWorkerAsync();
             foreach (MangaData manga in data)
             {
                 //Check if the "Manga" Dir exist. If not make a new one
@@ -38,23 +72,7 @@ namespace WeebLib.Manga.Retrieval
                     int imgNumber = 1;
                     foreach (string image in images)
                     {
-                        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(image);
-                        using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
-                        {
-                            using (Stream stream = response.GetResponseStream())
-                            {
-                                using (FileStream fileStream = new FileStream(Path.Combine(dir, $"{manga.name} - Chapter {manga.latestChapter}_{imgNumber}.jpg"), FileMode.Create, FileAccess.Write))
-                                {
-                                    byte[] buffer = new byte[4096];
-                                    int bytesRead;
-                                    do
-                                    {
-                                        bytesRead = stream.Read(buffer, 0, buffer.Length);
-                                        fileStream.Write(buffer, 0, bytesRead);
-                                    } while (bytesRead != 0);
-                                }
-                            }
-                        }
+                        DownloadImage(image, dir, manga, imgNumber);
                         ++imgNumber;
                     }
                 }
